d70ccd159fa547d959f55a1fa2f7c2c8bc966914
gustavoreche <gustavodreche@msn.com>
2024-01-05 19:26:00 -0300

    Implementa testes para modificacao no registra-atendimento

--- src/main/java/com/br/fiap/controller/VendedorController.java
+++ src/main/java/com/br/fiap/controller/VendedorController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,10 +30,13 @@ public class VendedorController {
 	private FilaAtendimentoRepository filaAtendimentoRepository;
 	
 	@PostMapping("/registra-atendimento")
-	public ResponseEntity<Void> registraAtendimento(@RequestBody @Valid AtendimentoDTO formulario) {
-		this.clienteRepository.save(formulario.cliente().converteParaCliente());
+	public ResponseEntity<Void> registraAtendimento(@RequestBody @Valid AtendimentoDTO formulario,
+			@RequestHeader LocalAcessadoEnum localAcessado) {
+		if(localAcessado.equals(LocalAcessadoEnum.SITE)) {
+			this.clienteRepository.save(formulario.cliente().converteParaCliente());
+			this.filaAtendimentoRepository.deleteById(formulario.pegaIdFilaAtendimento());
+		}
 		this.atendimentoRepository.save(formulario.converteParaAtendimento());
-		this.filaAtendimentoRepository.deleteById(formulario.pegaIdFilaAtendimento());
 		return ResponseEntity
 				.status(HttpStatus.CREATED)
 				.build();
--- src/test/java/com/br/fiap/RegistraAtendimentoTests.java
+++ src/test/java/com/br/fiap/RegistraAtendimentoTests.java
@@ -2,6 +2,7 @@ package com.br.fiap;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,8 +23,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.br.fiap.controller.VendedorController;
+import com.br.fiap.model.Cliente;
 import com.br.fiap.model.ClienteId;
 import com.br.fiap.model.FilaAtendimento;
+import com.br.fiap.model.FiltroDeBusca;
 import com.br.fiap.repository.AtendimentoRepository;
 import com.br.fiap.repository.ClienteRepository;
 import com.br.fiap.repository.FilaAtendimentoRepository;
@@ -60,8 +65,9 @@ class RegistraAtendimentoTests {
     	this.filaAtendimentoRepository.deleteAll();
     }
 
+
     @Test
-    public void shouldReturnStatus201() throws Exception {
+    public void shouldReturnStatus201_site() throws Exception {
 		var clienteId = new ClienteId();
 		clienteId.setNome("gustavo");
 		clienteId.setEmail("gustavo@teste.com");
@@ -97,6 +103,7 @@ class RegistraAtendimentoTests {
     	
         this.mockMvc
         	.perform(MockMvcRequestBuilders.post("/vendedor/registra-atendimento")
+        			.header("localAcessado", "SITE")
         			.contentType(MediaType.APPLICATION_JSON)
         			.content(request))
         	.andExpect(MockMvcResultMatchers
@@ -107,9 +114,69 @@ class RegistraAtendimentoTests {
         Assertions.assertEquals(1, this.clienteRepository.findAll().size());
         Assertions.assertEquals(1, this.atendimentoRepository.findAll().size());
     }
-   
+    
     @Test
-    public void shouldReturnStatus201_withMoreThanOneCustomer() throws Exception {
+    public void shouldReturnStatus201_estande() throws Exception {
+		var clienteId = new ClienteId();
+		clienteId.setNome("gustavo");
+		clienteId.setEmail("gustavo@teste.com");
+    	var filaAtendimento = new FilaAtendimento();
+		filaAtendimento.setAnoFiltroDeBusca("2020");
+		filaAtendimento.setModeloFiltroDeBusca("gol");
+		filaAtendimento.setCategoriaFiltroDeBusca("suv");		
+		filaAtendimento.setId(clienteId);
+		filaAtendimento.setTelefone("911223344");
+		filaAtendimento.setDataInsercao(LocalDateTime.now());
+    	this.filaAtendimentoRepository.save(filaAtendimento);
+    	
+		var filtroDeBusca = new FiltroDeBusca();
+		filtroDeBusca.setAno("2020");
+		filtroDeBusca.setModelo("gol");
+		filtroDeBusca.setCategoria("suv");
+    	var cliente = new Cliente();
+    	cliente.setId(clienteId);
+    	cliente.setTelefone("911223344");
+    	cliente.setFiltroDeBusca(filtroDeBusca);
+    	this.clienteRepository.save(cliente);
+    	
+        var request = """
+        		{
+        			"nome": "Anderson",
+        			"cliente": {
+					    "nome": "%s",
+					    "telefone": "%s",
+					    "email": "%s",
+					    "ano": "%s",
+					    "modelo": "%s",
+					    "categoria": "%s"
+					  }
+        		}
+        		""".formatted(
+        				clienteId.getNome(),
+        				filaAtendimento.getTelefone(),
+        				clienteId.getEmail(),
+        				filaAtendimento.getAnoFiltroDeBusca(),
+        				filaAtendimento.getModeloFiltroDeBusca(),
+        				filaAtendimento.getCategoriaFiltroDeBusca()
+        			);
+    	
+        this.mockMvc
+        	.perform(MockMvcRequestBuilders.post("/vendedor/registra-atendimento")
+        			.header("localAcessado", "ESTANDE")
+        			.contentType(MediaType.APPLICATION_JSON)
+        			.content(request))
+        	.andExpect(MockMvcResultMatchers
+        			.status()
+        			.isCreated()
+        			);
+        Assertions.assertEquals(1, this.filaAtendimentoRepository.findAll().size());
+        Assertions.assertEquals(1, this.clienteRepository.findAll().size());
+        Assertions.assertEquals(1, this.atendimentoRepository.findAll().size());
+    }
+   
+
+    @Test
+    public void shouldReturnStatus201_withMoreThanOneCustomer_site() throws Exception {
 		var clienteId = new ClienteId();
 		clienteId.setNome("gustavo");
 		clienteId.setEmail("gustavo@teste.com");
@@ -157,6 +224,7 @@ class RegistraAtendimentoTests {
     	
         this.mockMvc
         	.perform(MockMvcRequestBuilders.post("/vendedor/registra-atendimento")
+        			.header("localAcessado", "SITE")
         			.contentType(MediaType.APPLICATION_JSON)
         			.content(request))
         	.andExpect(MockMvcResultMatchers
@@ -167,10 +235,127 @@ class RegistraAtendimentoTests {
         Assertions.assertEquals(1, this.clienteRepository.findAll().size());
         Assertions.assertEquals(1, this.atendimentoRepository.findAll().size());
     }
-
     
     @Test
-    public void shouldReturnStatus400_withoutName() throws Exception {
+    public void shouldReturnStatus201_withMoreThanOneCustomer_estande() throws Exception {
+		var clienteId = new ClienteId();
+		clienteId.setNome("gustavo");
+		clienteId.setEmail("gustavo@teste.com");
+    	var filaAtendimento = new FilaAtendimento();
+		filaAtendimento.setAnoFiltroDeBusca("2020");
+		filaAtendimento.setModeloFiltroDeBusca("gol");
+		filaAtendimento.setCategoriaFiltroDeBusca("suv");		
+		filaAtendimento.setId(clienteId);
+		filaAtendimento.setTelefone("911223344");
+		filaAtendimento.setDataInsercao(LocalDateTime.now());
+
+		var clienteId2 = new ClienteId();
+		clienteId2.setNome("gustavo teste");
+		clienteId2.setEmail("teste@gustavo.com");
+    	var filaAtendimento2 = new FilaAtendimento();
+		filaAtendimento2.setAnoFiltroDeBusca("2024");
+		filaAtendimento2.setModeloFiltroDeBusca("onix");
+		filaAtendimento2.setCategoriaFiltroDeBusca("sedan");		
+		filaAtendimento2.setId(clienteId2);
+		filaAtendimento2.setTelefone("991919191");
+		filaAtendimento2.setDataInsercao(LocalDateTime.now().plusMinutes(1));
+		var lista = List.of(filaAtendimento, filaAtendimento2);
+    	this.filaAtendimentoRepository.saveAll(lista);
+    	
+		var filtroDeBusca = new FiltroDeBusca();
+		filtroDeBusca.setAno("2020");
+		filtroDeBusca.setModelo("gol");
+		filtroDeBusca.setCategoria("suv");
+    	var cliente = new Cliente();
+    	cliente.setId(clienteId);
+    	cliente.setTelefone("911223344");
+    	cliente.setFiltroDeBusca(filtroDeBusca);
+    	this.clienteRepository.save(cliente);
+    	
+        var request = """
+        		{
+        			"nome": "Anderson",
+        			"cliente": {
+					    "nome": "%s",
+					    "telefone": "%s",
+					    "email": "%s",
+					    "ano": "%s",
+					    "modelo": "%s",
+					    "categoria": "%s"
+					  }
+        		}
+        		""".formatted(
+        				clienteId.getNome(),
+        				filaAtendimento.getTelefone(),
+        				clienteId.getEmail(),
+        				filaAtendimento.getAnoFiltroDeBusca(),
+        				filaAtendimento.getModeloFiltroDeBusca(),
+        				filaAtendimento.getCategoriaFiltroDeBusca()
+        			);
+    	
+        this.mockMvc
+        	.perform(MockMvcRequestBuilders.post("/vendedor/registra-atendimento")
+        			.header("localAcessado", "ESTANDE")
+        			.contentType(MediaType.APPLICATION_JSON)
+        			.content(request))
+        	.andExpect(MockMvcResultMatchers
+        			.status()
+        			.isCreated()
+        			);
+        Assertions.assertEquals(2, this.filaAtendimentoRepository.findAll().size());
+        Assertions.assertEquals(1, this.clienteRepository.findAll().size());
+        Assertions.assertEquals(1, this.atendimentoRepository.findAll().size());
+    }
+
+    @Test
+    public void shouldReturnStatus400_withoutHeader() throws Exception {
+		var clienteId = new ClienteId();
+		clienteId.setNome("gustavo");
+		clienteId.setEmail("gustavo@teste.com");
+    	var filaAtendimento = new FilaAtendimento();
+		filaAtendimento.setAnoFiltroDeBusca("2020");
+		filaAtendimento.setModeloFiltroDeBusca("gol");
+		filaAtendimento.setCategoriaFiltroDeBusca("suv");		
+		filaAtendimento.setId(clienteId);
+		filaAtendimento.setTelefone("911223344");
+		filaAtendimento.setDataInsercao(LocalDateTime.now());
+    	this.filaAtendimentoRepository.save(filaAtendimento);
+    	
+        var request = """
+        		{
+        		 	"nome": "vendedor teste",	
+        			"cliente": {
+					    "nome": "%s",
+					    "telefone": "%s",
+					    "email": "%s",
+					    "ano": "%s",
+					    "modelo": "%s",
+					    "categoria": "%s"
+					  }
+        		}
+        		""".formatted(
+        				clienteId.getNome(),
+        				filaAtendimento.getTelefone(),
+        				clienteId.getEmail(),
+        				filaAtendimento.getAnoFiltroDeBusca(),
+        				filaAtendimento.getModeloFiltroDeBusca(),
+        				filaAtendimento.getCategoriaFiltroDeBusca()
+        			);
+    	
+				this.mockMvc
+				.perform(MockMvcRequestBuilders.post("/vendedor/registra-atendimento")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(request))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest()
+				);
+				Assertions.assertEquals(1, this.filaAtendimentoRepository.findAll().size());
+				Assertions.assertEquals(0, this.clienteRepository.findAll().size());
+				Assertions.assertEquals(0, this.atendimentoRepository.findAll().size());
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"SITE", "ESTANDE"})
+    public void shouldReturnStatus400_withoutName(String header) throws Exception {
 		var clienteId = new ClienteId();
 		clienteId.setNome("gustavo");
 		clienteId.setEmail("gustavo@teste.com");
@@ -203,22 +388,12 @@ class RegistraAtendimentoTests {
         				filaAtendimento.getCategoriaFiltroDeBusca()
         			);
     	
-        this.mockMvc
-        	.perform(MockMvcRequestBuilders.post("/vendedor/registra-atendimento")
-        			.contentType(MediaType.APPLICATION_JSON)
-        			.content(request))
-        	.andExpect(MockMvcResultMatchers
-        			.status()
-        			.isBadRequest()
-        			);
-        Assertions.assertEquals(1, this.filaAtendimentoRepository.findAll().size());
-        Assertions.assertEquals(0, this.clienteRepository.findAll().size());
-        Assertions.assertEquals(0, this.atendimentoRepository.findAll().size());
+        this.assertBadRequest(request, header);
     }
     
     @ParameterizedTest
-    @ValueSource(strings = {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "aa"})
-    public void shouldReturnStatus400_withNamesInvalids(String name) throws Exception {
+    @MethodSource("headerAndName")
+    public void shouldReturnStatus400_withNamesInvalids(String header, String name) throws Exception {
 		var clienteId = new ClienteId();
 		clienteId.setNome("gustavo");
 		clienteId.setEmail("gustavo@teste.com");
@@ -253,17 +428,31 @@ class RegistraAtendimentoTests {
         				filaAtendimento.getCategoriaFiltroDeBusca()
         			);
     	
-        this.mockMvc
-        	.perform(MockMvcRequestBuilders.post("/vendedor/registra-atendimento")
-        			.contentType(MediaType.APPLICATION_JSON)
-        			.content(request))
-        	.andExpect(MockMvcResultMatchers
-        			.status()
-        			.isBadRequest()
-        			);
-        Assertions.assertEquals(1, this.filaAtendimentoRepository.findAll().size());
-        Assertions.assertEquals(0, this.clienteRepository.findAll().size());
-        Assertions.assertEquals(0, this.atendimentoRepository.findAll().size());
+        this.assertBadRequest(request, header);
     }
+    
+	private void assertBadRequest(String request, String header) throws Exception {
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.post("/vendedor/registra-atendimento")
+						.header("localAcessado", header)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(request))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest()
+			);
+		Assertions.assertEquals(1, this.filaAtendimentoRepository.findAll().size());
+		Assertions.assertEquals(0, this.clienteRepository.findAll().size());
+		Assertions.assertEquals(0, this.atendimentoRepository.findAll().size());
+	}
+	
+	private static Stream<Arguments> headerAndName() {
+	    return Stream.of(
+	            Arguments.of("SITE", "aa"),
+	            Arguments.of("SITE", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
+	            Arguments.of("SITE", " "),
+	            Arguments.of("ESTANDE", "aa"),
+	            Arguments.of("ESTANDE", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
+	            Arguments.of("ESTANDE", " ")
+	    );
+	}
 
 }
